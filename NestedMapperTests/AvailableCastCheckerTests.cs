457ce96dd4866b9fce25f167210d394883a58186
Brann Joly <joly.brann@gmail.com>
2015-08-15 18:25:20 +0200

    added AvailableCastCheckerTests

---
+++ NestedMapperTests/AvailableCastCheckerTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Dynamic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NestedMapper;
+using NFluent;
+
+namespace NestedMapperTests
+{
+    [TestClass]
+    public class AvailableCastCheckerTests
+    {
+
+
+
+        [TestMethod]
+        public void HasImplicitConversion_Works_On_Builtin_Types()
+        {
+            Check.That(AvailableCastChecker.CanCast(typeof (int), typeof (decimal))).IsTrue();
+        }
+
+
+    }
+}
